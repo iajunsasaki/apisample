6d084d7847c85e24f35f2673bf0432ab7a540711
Jun Sasaki <iajunsasaki@gmail.com>
2024-05-15 09:19:28 +0900

    履歴取得追加

--- src/main/java/com/example/demo/PaperRockScissorController.java
+++ src/main/java/com/example/demo/PaperRockScissorController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,15 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaperRockScissorController {
+	private static List<Hand> handHistory = new ArrayList()<Hand>();
+	
 	@GetMapping("/")
 	@CrossOrigin
 	public Hand index(Model model) {
 		Hand hand = PaperRockScissor.playPaperRockScissor();
-		model.addAttribute("title", "じゃんけんポン！!!!!");
-		model.addAttribute("msg", hand);
-		model.addAttribute("time", new Date());
 		System.out.println(hand);
+		handHistory.add(hand);
 		return hand;
 	}
 
+	@GetMapping("/history")
+	@CrossOrigin
+	public List<Hand> history(Model model) {
+		return handHistory;
+	}
 }
